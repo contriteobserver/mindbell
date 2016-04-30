1364e34a96f435959da91887f49ebd71c2dbded6
uwe <uwe@DX1704>
2016-04-30 09:34:25 +0200

    Copyright updated and release set to 2.5.2

--- mindbell-instrumentationtest/src/main/java/de/dknapps/mindbell/test/accessors/MockContextAccessor.java
+++ mindbell-instrumentationtest/src/main/java/de/dknapps/mindbell/test/accessors/MockContextAccessor.java
@@ -1,18 +1,22 @@
-/*
- * Copyright (C) 2010 Marc Schroeder.
+/*******************************************************************************
+ * MindBell - Aims to give you a support for staying mindful in a busy life -
+ *            for remembering what really counts
+ *
+ *     Copyright (C) 2010-2014 Marc Schroeder
+ *     Copyright (C) 2014-2016 Uwe Damken
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */
+ *******************************************************************************/
 package de.dknapps.mindbell.test.accessors;
 
 import java.util.Timer;
