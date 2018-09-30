814154f35cfc34aee925371063b2cabaa845198c
uwe <uwe@DX1704>
2018-09-30 11:17:55 +0200

    Tests improved (now working for all API levels), minTargetSdk=21 because emulator for API level 19 does not contain any system sounds that are needed to pass the settings tests.

--- app/src/androidTest/java/com/googlecode/mindbell/activity/SettingsActivityTest.kt
+++ app/src/androidTest/java/com/googlecode/mindbell/activity/SettingsActivityTest.kt
@@ -2,7 +2,7 @@
  * MindBell - Aims to give you a support for staying mindful in a busy life -
  *            for remembering what really counts
  *
- *     Copyright (C) 2014-2016 Uwe Damken
+ *     Copyright (C) 2014-2018 Uwe Damken
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -19,11 +19,13 @@
 
 package com.googlecode.mindbell.activity
 
+import android.R
 import android.R.id.summary
 import android.R.id.title
 import android.content.Context
 import android.content.res.Resources
 import android.media.AudioManager
+import android.os.Build
 import android.support.test.InstrumentationRegistry
 import android.support.test.espresso.Espresso.onData
 import android.support.test.espresso.Espresso.onView
@@ -36,6 +38,8 @@ import android.support.test.espresso.matcher.PreferenceMatchers
 import android.support.test.espresso.matcher.ViewMatchers.*
 import android.support.test.rule.ActivityTestRule
 import android.support.test.runner.AndroidJUnit4
+import android.support.test.uiautomator.UiDevice
+import android.support.test.uiautomator.UiSelector
 import android.widget.TimePicker
 import com.googlecode.mindbell.R.array.*
 import com.googlecode.mindbell.R.id.iconText
@@ -53,11 +57,13 @@ import junit.framework.Assert.*
 import org.hamcrest.Matchers
 import org.hamcrest.Matchers.allOf
 import org.hamcrest.Matchers.not
+import org.junit.After
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 import java.util.*
+
 
 @RunWith(AndroidJUnit4::class)
 class SettingsActivityTest {
@@ -77,11 +83,17 @@ class SettingsActivityTest {
         prefs = Prefs.getInstance(context)
         prefs.resetSettings()
         resources = context.resources
-        activityTestRule.launchActivity(null) // launch activity now to get reset preferences
+        // launch activity in test methods to allow more preferences to be changed before
+    }
+
+    @After
+    fun tearDown() {
+        InstrumentationRegistry.getInstrumentation().uiAutomation.executeShellCommand("pm revoke ${InstrumentationRegistry.getTargetContext().packageName} android.permission.READ_EXTERNAL_STORAGE")
     }
 
     @Test
     fun testActivityIsDisplayed() {
+        activityTestRule.launchActivity(null) // launch activity now to get reset preferences
         onView(withText(preferencesTitle)).check(matches(isDisplayed()))
     }
 
@@ -89,6 +101,7 @@ class SettingsActivityTest {
     fun testSoundOutputPreferencesPage() {
 
         // Navigate to sound output preferences page
+        activityTestRule.launchActivity(null) // launch activity now to get reset preferences
         onView(allOf(withId(title), withText(prefsCategorySoundOutput))).perform(click())
         onView(allOf(withText(prefsCategorySoundOutput), not(withText(preferencesTitle)))).check(matches(isDisplayed()))
 
@@ -157,6 +170,7 @@ class SettingsActivityTest {
     fun testActiveTimesPreferencesPage() {
 
         // Navigate to sound output preferences page
+        activityTestRule.launchActivity(null) // launch activity now to get reset preferences
         onView(allOf(withId(title), withText(prefsCategoryActiveTimes))).perform(click())
         onView(allOf(withText(prefsCategoryActiveTimes), not(withText(preferencesTitle)))).check(matches(isDisplayed()))
 
@@ -192,7 +206,7 @@ class SettingsActivityTest {
         preferenceStart.perform(ViewActions.scrollTo(), click())
         onView(allOf(isDialogTitle(), withText(prefsStart))).check(matches(isDisplayed()))
         onView(withClassName(Matchers.equalTo(TimePicker::class.java.name))).perform(PickerActions.setTime(11, 12))
-        onView(allOf(withId(android.R.id.button1), withText(android.R.string.ok))).perform(click())
+        onView(allOf(withId(R.id.button1), withText(R.string.ok))).perform(click())
         preferenceStart.onChildView(withId(summary)).check(matches(withText(TimeOfDay(11, 12).getDisplayString(context))))
         assertEquals(TimeOfDay(11, 12), prefs.daytimeStart)
 
@@ -200,12 +214,12 @@ class SettingsActivityTest {
         preferenceEnd.perform(ViewActions.scrollTo(), click())
         onView(allOf(isDialogTitle(), withText(prefsEnd))).check(matches(isDisplayed()))
         onView(withClassName(Matchers.equalTo(TimePicker::class.java.name))).perform(PickerActions.setTime(23, 24))
-        onView(allOf(withId(android.R.id.button1), withText(android.R.string.ok))).perform(click())
+        onView(allOf(withId(R.id.button1), withText(R.string.ok))).perform(click())
         preferenceEnd.onChildView(withId(summary)).check(matches(withText(TimeOfDay(23, 24).getDisplayString(context))))
         assertEquals(TimeOfDay(23, 24), prefs.daytimeEnd)
 
         // Choose only Monday
-        preferenceActiveOnDaysOfWeek.perform(click())  // open selection
+        preferenceActiveOnDaysOfWeek.perform(ViewActions.scrollTo(), click())  // open selection
         onView(allOf(isDialogTitle(), withText(prefsActiveOnDaysOfWeek))).check(matches(isDisplayed()))
         for (i in 0..6) {
             onView(withText(resources.getStringArray(weekdayEntries)[i])).perform(ViewActions.scrollTo(), click()) // un-choose all days
@@ -213,7 +227,7 @@ class SettingsActivityTest {
         val mondaySet = HashSet(Arrays.asList("2"))
         val mondayWeekSummary = Utils.deriveOrderedEntrySummary(mondaySet, resources.getStringArray(weekdayEntries), resources.getStringArray(weekdayEntryValues))
         onView(withText(mondayWeekSummary)).perform(click()) // choose only Monday
-        onView(allOf(withId(android.R.id.button1), withText(android.R.string.ok))).perform(click())
+        onView(allOf(withId(R.id.button1), withText(R.string.ok))).perform(click())
         preferenceActiveOnDaysOfWeek.onChildView(withId(summary)).check(matches(withText(mondayWeekSummary)))
         assertEquals(mondaySet.toString(), prefs.activeOnDaysOfWeek.toString())
         onToast(atLeastOneActiveDayNeeded).check(doesNotExist())
@@ -222,7 +236,7 @@ class SettingsActivityTest {
         preferenceActiveOnDaysOfWeek.perform(click())  // open selection
         onView(allOf(isDialogTitle(), withText(prefsActiveOnDaysOfWeek))).check(matches(isDisplayed()))
         onView(withText(mondayWeekSummary)).perform(click()) // de-choose also Monday
-        onView(allOf(withId(android.R.id.button1), withText(android.R.string.ok))).perform(click())
+        onView(allOf(withId(R.id.button1), withText(R.string.ok))).perform(click())
         checkDisplayedAndDisappearedOnToast(atLeastOneActiveDayNeeded)
         preferenceActiveOnDaysOfWeek.onChildView(withId(summary)).check(matches(withText(mondayWeekSummary)))
         assertEquals(mondaySet.toString(), prefs.activeOnDaysOfWeek.toString())
@@ -231,7 +245,7 @@ class SettingsActivityTest {
         preferenceFrequency.perform(ViewActions.scrollTo(), click())
         onView(allOf(isDialogTitle(), withText(prefsFrequency))).check(matches(isDisplayed()))
         onView(withClassName(Matchers.equalTo(TimePicker::class.java.name))).perform(PickerActions.setTime(0, 20))
-        onView(allOf(withId(android.R.id.button1), withText(android.R.string.ok))).perform(click())
+        onView(allOf(withId(R.id.button1), withText(R.string.ok))).perform(click())
         preferenceFrequency.onChildView(withId(summary)).check(matches(withText("00:20 (20 min)")))
         assertEquals(20 * ONE_MINUTE_MILLIS, prefs.interval)
 
@@ -255,7 +269,7 @@ class SettingsActivityTest {
         preferenceFrequency.perform(ViewActions.scrollTo(), click())
         onView(allOf(isDialogTitle(), withText(prefsFrequency))).check(matches(isDisplayed()))
         onView(withClassName(Matchers.equalTo(TimePicker::class.java.name))).perform(PickerActions.setTime(1, 7))
-        onView(allOf(withId(android.R.id.button1), withText(android.R.string.ok))).perform(click())
+        onView(allOf(withId(R.id.button1), withText(R.string.ok))).perform(click())
         checkDisplayedAndDisappearedOnToast(frequencyDoesNotFitIntoAnHour)
         preferenceFrequency.onChildView(withId(summary)).check(matches(withText("00:20 (20 min)")))
         assertEquals(20 * ONE_MINUTE_MILLIS, prefs.interval)
@@ -271,7 +285,7 @@ class SettingsActivityTest {
         preferenceFrequency.perform(ViewActions.scrollTo(), click())
         onView(allOf(isDialogTitle(), withText(prefsFrequency))).check(matches(isDisplayed()))
         onView(withClassName(Matchers.equalTo(TimePicker::class.java.name))).perform(PickerActions.setTime(1, 7))
-        onView(allOf(withId(android.R.id.button1), withText(android.R.string.ok))).perform(click())
+        onView(allOf(withId(R.id.button1), withText(R.string.ok))).perform(click())
         preferenceFrequency.onChildView(withId(summary)).check(matches(withText("01:07 (67 min)")))
         assertEquals(67 * ONE_MINUTE_MILLIS, prefs.interval)
 
@@ -283,7 +297,7 @@ class SettingsActivityTest {
         preferenceFrequency.perform(ViewActions.scrollTo(), click())
         onView(allOf(isDialogTitle(), withText(prefsFrequency))).check(matches(isDisplayed()))
         onView(withClassName(Matchers.equalTo(TimePicker::class.java.name))).perform(PickerActions.setTime(0, 20))
-        onView(allOf(withId(android.R.id.button1), withText(android.R.string.ok))).perform(click())
+        onView(allOf(withId(R.id.button1), withText(R.string.ok))).perform(click())
         preferenceFrequency.onChildView(withId(summary)).check(matches(withText("00:20 (20 min)")))
         assertEquals(20 * ONE_MINUTE_MILLIS, prefs.interval)
 
@@ -306,6 +320,7 @@ class SettingsActivityTest {
     fun testReminderActionsPreferencesPage() {
 
         // Navigate to sound output preferences page
+        activityTestRule.launchActivity(null) // launch activity now to get reset preferences
         onView(allOf(withId(title), withText(prefsCategoryReminderActions))).perform(click())
         onView(allOf(withText(prefsCategoryReminderActions), not(withText(preferencesTitle)))).check(matches(isDisplayed()))
 
@@ -355,6 +370,77 @@ class SettingsActivityTest {
         preferenceReminderBell.check(matches(withText(resources.getStringArray(reminderBellEntries)[2])))
         assertEquals(prefs.getBellSoundUri("2"), prefs.getReminderSoundUri())
 
+        // Try to change volume
+        preferenceVolume.perform(ViewActions.scrollTo(), click())
+        onView(allOf(isDialogTitle(), withText(prefsVolume))).check(doesNotExist())
+
+        // Try to choose ringtone
+        preferenceRingtone.perform(ViewActions.scrollTo(), click())
+        onView(allOf(isDialogTitle(), withText(prefsRingtone))).check(doesNotExist())
+
+        // Choose reminder bell system sound
+        preferenceReminderBell.perform(ViewActions.scrollTo(), click())
+        onView(allOf(isDialogTitle(), withText(prefsReminderBell))).check(matches(isDisplayed()))
+        onData(Matchers.`is`(resources.getStringArray(reminderBellEntries)[0])).perform(ViewActions.scrollTo(), click())
+        allowPermission()
+        preferenceReminderBell.check(matches(withText(resources.getStringArray(reminderBellEntries)[0])))
+        assertEquals(prefs.getDefaultReminderBellSoundUri(), prefs.getReminderSoundUri()) // no ringtone chosen => default
+
+        // Choose ringtone
+        preferenceRingtone.perform(ViewActions.scrollTo(), click())
+        pressText("Adara") // could not find a way to choose a sound independent of the name
+        pressText("OK") // pressButton(1) does not work after sound selection, no idea why not
+        preferenceRingtone.check(matches(withText("Adara")))
+
+    }
+
+    @Test
+    fun testReminderActionsPreferencesPage_isUseAudioStreamVolumeOff() {
+
+        // Switch OFF isUseAudioStreamVolumeSetting and check in UI and Prefs
+        prefs.isUseAudioStreamVolumeSetting = false
+
+        // Navigate to sound output preferences page
+        activityTestRule.launchActivity(null) // launch activity now to get reset preferences
+        onView(allOf(withId(title), withText(prefsCategoryReminderActions))).perform(click())
+        onView(allOf(withText(prefsCategoryReminderActions), not(withText(preferencesTitle)))).check(matches(isDisplayed()))
+
+        // Fetch text views of active times preferences page
+        val preferenceVolume = onData(PreferenceMatchers.withKey(resources.getString(keyVolume)))
+
+        // Open volume setting and press OK (cannot move the slider)
+        preferenceVolume.perform(ViewActions.scrollTo(), click())
+        onView(allOf(isDialogTitle(), withText(prefsVolume))).check(matches(isDisplayed()))
+        onView(allOf(withId(R.id.button1), withText(R.string.ok))).perform(click())
+
+        // Open volume setting and press CANCEL
+        preferenceVolume.perform(ViewActions.scrollTo(), click())
+        onView(allOf(isDialogTitle(), withText(prefsVolume))).check(matches(isDisplayed()))
+        onView(allOf(withId(R.id.button2), withText(R.string.cancel))).perform(click())
+
+    }
+
+    private fun allowPermission() {
+        if (Build.VERSION.SDK_INT >= 23) {
+            pressButton(1)
+        }
+    }
+
+    /**
+     * Press a button with the given index or fail with failMessage if its not there.
+     *
+     * Following David's comment to https://stackoverflow.com/a/34947797/2532583
+     */
+    private fun pressButton(index: Int) {
+        val clickableUiObject = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation()).findObject(UiSelector().clickable(true).index(index))
+        assertTrue("Expected button with index $index does not exist", clickableUiObject.exists())
+        clickableUiObject.click()
+    }
+
+    private fun pressText(text: String) {
+        val clickableUiObject = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation()).findObject(UiSelector().text(text))
+        assertTrue("Expected text $text does not exist", clickableUiObject.exists())
+        clickableUiObject.click()
     }
 
 }
